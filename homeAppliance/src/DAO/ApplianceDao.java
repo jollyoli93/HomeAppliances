338e1720cd619c56c770b910c6564fd28a890ef5
Jackoliver1993 <106276990+Jackoliver1993@users.noreply.github.com>
2024-10-20 13:00:19 +0100

    HomeAppliance type added to DAO extension

--- homeAppliance/src/DAO/ApplianceDao.java
+++ homeAppliance/src/DAO/ApplianceDao.java
@@ -1,6 +1,8 @@
 package DAO;
 
-public class ApplianceDao extends DAO {
+import homeApplianceStore.HomeAppliance;
+
+public class ApplianceDao extends DAO<HomeAppliance> {
 	String path;
 	
 	public ApplianceDao(String path) {
