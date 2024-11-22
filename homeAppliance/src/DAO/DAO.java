53315549f3fefd9c881063237dc1a3b4afb05d4e
jackoliver <jackoliver@localhost>
2024-11-22 21:56:04 +0000

    added create and check table methods to the DAO

--- homeAppliance/src/DAO/DAO.java
+++ homeAppliance/src/DAO/DAO.java
@@ -1,4 +1,8 @@
 package DAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public abstract class DAO<T> {
@@ -17,12 +21,61 @@ public abstract class DAO<T> {
 		
 	}
 	
+	
+	public boolean createTable (String name) {
+		String query = "CREATE TABLE " + name
+				+"( id	INTEGER NOT NULL UNIQUE,"
+				+"sku	TEXT NOT NULL,"
+				+"description	TEXT NOT NULL,"
+				+" category	TEXT NOT NULL,"
+				+"price	INTEGER NOT NULL,"
+				+"PRIMARY KEY(id AUTOINCREMENT))";
+		
+		try (Connection connect = connector.initializeDBConnection();
+			 PreparedStatement preparedStatement = connect.prepareStatement(query)) {
+			
+			int updated = preparedStatement.executeUpdate();
+			
+			System.out.println("Table test created");
+			
+	        return updated > 0;
+		} catch (SQLException e) {
+			System.out.println("Error connecting to the database");
+            System.out.println("SQL Exception: " + e.getMessage());
+            return false;
+		}
+	}
+
+
+	public boolean checkTableExists(String name) {
+		String query = "SELECT name FROM sqlite_master WHERE type='table' AND name='" + name + "' COLLATE NOCASE";
+
+		
+		try (Connection connect = connector.initializeDBConnection();
+			 PreparedStatement preparedStatement = connect.prepareStatement(query)) {
+			
+			ResultSet result = preparedStatement.executeQuery();
+			
+			if (result.next()) {
+				System.out.println("table exists.");
+				return true;
+			}
+				
+		} catch (SQLException e) {
+			System.out.println("Error connecting to the database");
+            System.out.println("SQL Exception: " + e.getMessage());
+            return false;
+		}
+		
+		return false;
+		
+	}
+	
 	public abstract ArrayList<T> findAll();
 	public abstract T getById(int id);
 	public abstract boolean addNew(T add);
 	public abstract boolean deleteById(int id);
 	public abstract boolean updateById(int id, Object update);
-	protected abstract boolean createTable(String name);
 
 
 }
