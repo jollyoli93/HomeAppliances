6938485f9e7db1d66fd78d77ba58fccdd0aa282d
jackoliver <jackoliver@localhost>
2024-11-22 21:12:09 +0000

    successful test table created

--- homeAppliance/src/homeApplianceStore/HomeApplianceStore.java
+++ homeAppliance/src/homeApplianceStore/HomeApplianceStore.java
@@ -2,15 +2,18 @@ package homeApplianceStore;
 
 import java.util.ArrayList;
 
+import DAO.ApplianceDao;
+
 public class HomeApplianceStore {
 	public static void main(String[] args ) {
-	ApplianceFactory entertainment = new EntertainmentFactory();
-		
-		Appliance tv = entertainment.selectAppliance("Basic Television");
-		
-		ArrayList<String> list = entertainment.listAllApplianceTypes();
-		
-		System.out.println(list.get(0));
-
+//	ApplianceFactory entertainment = new EntertainmentFactory();
+//		
+//		Appliance tv = entertainment.selectAppliance("Basic Television");
+//		
+//		ArrayList<String> list = entertainment.listAllApplianceTypes();
+//		
+//		System.out.println(list.get(0));
+		ApplianceDao testTable = new ApplianceDao("HomeAppliances", null);
+		testTable.createTable("applianceTest");
 	}
 }
