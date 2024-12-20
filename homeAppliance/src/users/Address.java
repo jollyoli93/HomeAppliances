e8356bda8481dde4a7355433c4c5cfcdb42d8b49
jackoliver <jackoliver@localhost>
2024-12-20 20:55:08 +0000

    get and setter methods added to address class

--- homeAppliance/src/users/Address.java
+++ homeAppliance/src/users/Address.java
@@ -1,58 +1,86 @@
 package users;
 
 public abstract class Address {
-	private int customerId;
-	private int number;
-	private String street;
-	private String city;
-	private String postCode;
-	private String country;
-	private boolean mainAddress;
-	private String addressType;
+    private int address_id;
+    private int customerId;
+    private String number;
+    private String street;
+    private String city;
+    private String postCode;
+    private String country;
+    private boolean isPrimary;
+    private String addressType;
 
-	
-	public void setAddress(int number, String street, String city, String country, String postCode, int customerId) {
-		this.number = number;
-		this.street = street;
-		this.city = city;
-		this.postCode = postCode;
-		this.customerId = customerId;
-	}
-	
-	public int getCustomerId() {
-	    return this.customerId;
-	}
+    // Constructor
+    protected Address(String number, String street, String city, String country, String postCode, int customerId) {
+        setAddress(number, street, city, country, postCode, customerId);
+    }
 
-	
-	public void setMainAddress(boolean main) {
-		this.mainAddress = main;
-	}
-	
-	public void setAddressType(String type) {
-		this.addressType = type;
-	}
-	
+    public void setAddress(String number, String street, String city, String country, String postCode, int customerId) {
+        if (number == null || street == null || city == null || country == null || postCode == null) {
+            throw new IllegalArgumentException("Address fields cannot be null.");
+        }
+        this.number = number;
+        this.street = street;
+        this.city = city;
+        this.country = country;
+        this.postCode = postCode;
+        this.customerId = customerId;
+    }
+
+    public int getAddress_id() {
+        return address_id;
+    }
+
+    public void setAddress_id(int address_id) {
+        this.address_id = address_id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public boolean isPrimary() {
+        return isPrimary;
+    }
+
+    public void setMainAddress(boolean isPrimary) {
+        this.isPrimary = isPrimary;
+    }
+
+    public String getAddressType() {
+        return addressType;
+    }
+
+    protected void setAddressType(String addressType) {
+        this.addressType = addressType;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
     public String toString() {
-        return number + " " + street + ", " + city + " " + postCode;
+        return addressType + " Address: " + number + " " + street + ", " + city + " " + postCode + ", " + country;
     }
 }
 
 class ShippingAddress extends Address {
-	public ShippingAddress(int number, String street, String city, String country, String postCode, int customerId) {
-	    setAddress(number, street, city, country, postCode, customerId);
-	    setAddressType("Shipping");
-	}
-
+    public ShippingAddress(String number, String street, String city, String country, String postCode, int customerId) {
+        super(number, street, city, country, postCode, customerId);
+        setAddressType("Shipping");
+    }
 }
 
 class BillingAddress extends Address {
-	public BillingAddress(int number, String street, String city, String country, String postCode, int customerId) {
-	    setAddress(number, street, city, country, postCode, customerId);
-	    setAddressType("Billing");
-	}
-
+    public BillingAddress(String number, String street, String city, String country, String postCode, int customerId) {
+        super(number, street, city, country, postCode, customerId);
+        setAddressType("Billing");
+    }
 }
 
-//class BuisnessAddress {
-//	
-//}
